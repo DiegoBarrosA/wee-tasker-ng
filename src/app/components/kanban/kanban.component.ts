79f0b6ccb0abf1d856cefb84581bcd12742eed84
DiegoBarrosA <diegobarrosaraya@outlook.com>
2024-11-23 15:18:35 -0300

    Adding login page

---
+++ src/app/components/kanban/kanban.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../common/navbar/navbar.component';
+@Component({
+  selector: 'app-kanban',
+  imports: [NavbarComponent],
+  templateUrl: './kanban.component.html',
+  styleUrl: './kanban.component.css'
+})
+export class KanbanComponent {
+
+}
