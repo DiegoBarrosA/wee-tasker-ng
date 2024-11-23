73f9a07aab63524eda49eceae63f82ea82089065
DiegoBarrosA <diegobarrosaraya@outlook.com>
2024-11-23 17:45:23 -0300

    Compodoc added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 
-
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
+  getErrorMessage(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (control && control.errors) {
+      if (control.errors['required']) {
+        return `${controlName} is required.`;
+      } else if (control.errors['minlength']) {
+        const requiredLength = control.errors['minlength'].requiredLength;
+        return `${controlName} must be at least ${requiredLength} characters long.`;
+      } else if (control.errors['email']) {
+        return 'Invalid email format.';
+      }
+    }
+    return '';
+  }
+  submitForm() {
+    if (this.loginForm.valid) {
+      console.log('Look! ' + this.loginForm.get('email')!.value);
+    } else {
+      console.log('Nope');
+    }
+    return null;
+  }
 }
